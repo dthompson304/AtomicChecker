104ce9d7d7d594b5903ebae195822791e1a10bad
Thompson, Daniel <dthompson304@gatech.edu>
2023-11-26 20:37:15 -0500

    commented out jesses broken code, added in --ag option for getting all grades of a card that are in stock. also fixed so all cards are serached not just ones with dropdown

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 import nest_asyncio
+import os
 import pandas as pd
 
 from discord.ext import commands
@@ -12,7 +13,10 @@ nest_asyncio.apply()
 # Set up Discord intents
 intents = discord.Intents.default()
 intents.messages = True
-intents.message_content = True
+
+#get WD
+current_working_directory = os.getcwd()
+print("Current Working Directory:", current_working_directory)
 
 # Initialize the Discord bot
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -42,16 +46,23 @@ async def check_cards(ctx, *, card_names):
     else:
         await ctx.send("Request received Daddy. Loud and Proud searching for cards ;)...")
 
+    all_grades = '--ag' in card_names
+    if all_grades:
+        await ctx.send("Daddy got $$ sending you all stock options for this card :) :)")
+        card_names = card_names.replace('--ag', '').strip()
+    else:
+        await ctx.send("We love a daddy who saves his money for me, only checking MP and below grading")
+
     try:
         results = []
         if card_names.lower() == 'all':
             await ctx.send("You have a big request daddy, please wait for me patiently xoxo")
-            file_path = 'lists/cards.txt'
+            file_path = "Atmoic Checker\lists\cards.txt"
             with open(file_path, 'r') as file:
                 for line in file:
                     card_name = line.strip()
                     if card_name:
-                        result = atomic.search_card(card_name, quiet=quiet_mode)
+                        result = atomic.search_card(card_name, quiet=quiet_mode, all_grades=all_grades)
                         if result:
                             results.append(result.strip())
         else:
@@ -60,15 +71,23 @@ async def check_cards(ctx, *, card_names):
             if list_size > 14:
                  await ctx.send("You have a big request daddy, please wait for me patiently xoxo")
             for card_name in card_names_list:
-                result = atomic.search_card(card_name, quiet=quiet_mode)
+                result = atomic.search_card(card_name, quiet=quiet_mode,  all_grades=all_grades)
                 if result:
                     results.append(result.strip())
 
         if results:
             response = "\n".join(results)
-            await ctx.send(response)
+            # Check if response is too long and split if necessary
+            while len(response) > 0:
+                if len(response) > 2000:
+                    part, response = response[:2000], response[2000:]
+                    await ctx.send(part)
+                else:
+                    await ctx.send(response)
+                    break
         else:
             await ctx.send("No results found.")
+
 
     except Exception as e:
         await ctx.send(f"An error occurred: {e}")
@@ -76,37 +95,7 @@ async def check_cards(ctx, *, card_names):
         command_in_progress = False
         await ctx.send("Search complete Senpai. I hope youre happy and use me again like the bad bot I am! xoxo <2<3<3")
 
-
-async def watchdog(ctx):
-    await bot.wait_until_ready()
-    
-    while not bot.is_closed():
-        try:
-            results = []
-
-            # Get Card Metadata
-            cards_df = pd.read_csv('lists/cards_v2.csv', header=True)
-            cards_dicts = cards_df.to_dict('records')
-
-            # Loop and check to see if card is below threshold
-            for cards_dict in cards_dicts:
-                result, price, grade = atomic.search_card(cards_dict['name'])
-                if result and cards_dict['grade'] in grade and price <= cards_dict['threshold']:
-                    results.append(result.strip())
-
-            if results:
-                response = "\n".join(results)
-                await ctx.send('🚨 I found something you might like Senpai.. are you proud of me? 🚨\n' + response)
-
-        except Exception as e:
-            print(f"Error: {e}")
-
-        # Wait until next scrape
-        await asyncio.sleep(2 * 60 * 60) # 2 hours
-
-
-bot.loop.create_task(watchdog())
-bot.run('')
+bot.run('MTE3NjYxODg1MTk2NjkyMjg2Ng.GeBNfd.SUiJf2g3TvzWHc_EO5RXYSj-5EeVsfGYJBBNIM')
             
     
 
--- scrapers/atomic.py
+++ scrapers/atomic.py
@@ -2,9 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.parse
 
-def search_card(card_name, quiet=False):
+def search_card(card_name, quiet=False, all_grades=False):
     encoded_card_name = urllib.parse.quote_plus(card_name)
     search_url = f"https://www.atomicempire.com/Card/List?txt={encoded_card_name}"
+    print(card_name)
+    color_s = '--------' in card_name
+    if color_s:
+        print(card_name)
+        return card_name
     response = requests.get(search_url)
 
     output_text = ""  # String to accumulate results
@@ -18,27 +23,34 @@ def search_card(card_name, quiet=False):
             if "(Not Tournament Legal)" in original_card_title:
                 continue
 
+            # Use the original card title in quiet mode
+            card_title = card_name if quiet else original_card_title
+
+            # Include set info only if not in quiet mode
+            card_set_tag = item.find('a', href=lambda href: href and "set=" in href)
+            card_set = card_set_tag.text.strip() if card_set_tag else 'Unknown Set'
+
+            # Handle multiple grade variant
             card_grades = item.find('select', class_='itemid')
             if card_grades:
                 options = card_grades.find_all('option')
                 for option in options:
                     grade = option.text.strip()
-                    if "SP/NM" not in grade:
-                        price = float(grade.split('- $')[1]) if '- $' in grade else 0
-                        if price >= 5:
-                            # Use the original card title in quiet mode
-                            card_title = card_name if quiet else original_card_title
-                            # Include set info only if not in quiet mode
-                            if not quiet:
-                                card_set_tag = item.find('a', href=lambda href: href and "set=" in href)
-                                card_set = card_set_tag.text.strip() if card_set_tag else 'Unknown Set'
-                                result = f"{card_title}, Set: {card_set}, Grade: {grade}\n"
-                            else:
-                                result = f"{card_name}, Grade: {grade}\n"
-                            # print(result, end='')
-                            output_text += result
+                    price = float(grade.split('- $')[1]) if '- $' in grade else 0
+                    if price >= 5 and (all_grades or "SP/NM" not in grade):
+                        result = f"{card_title}, Set: {card_set}, Grade: {grade}\n" if not quiet else f"{card_name}, Grade: {grade}\n"
+                        output_text += result
 
+            # Handle single grade variant
+            else:
+                single_grade_price = item.find('strong')
+                if single_grade_price:
+                    grade = single_grade_price.text.strip()
+                    price = float(grade.split('- $')[1]) if '- $' in grade else 0
+                    if price >= 5 and (all_grades or "SP/NM" not in grade):
+                        result = f"{card_title}, Set: {card_set}, Grade: {grade}\n" if not quiet else f"{card_name}, Grade: {grade}\n"
+                        output_text += result
     else:
         print("Failed to retrieve information")
 
-    return output_text.strip(), price, grade
+    return output_text.strip()
